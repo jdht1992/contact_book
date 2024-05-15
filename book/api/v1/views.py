d7688cfd9d78a1bf029479a6a7abd906c2379ea2
Diego H <jdht1992@gmail.com>
2024-05-14 18:32:48 -0600

    separation of applications

--- book/api/v1/views.py
+++ book/api/v1/views.py
@@ -18,7 +18,11 @@ from book.models import Person, Phone
 
 class PersonModelViewSet(ModelViewSet):
     """
-    A viewset for viewing and editing user instances.
+    The ModelViewSet class inherits from GenericAPIView and includes implementations for various actions, 
+    by mixing in the behavior of the various mixin classes.
+
+    The actions provided by the ModelViewSet class are .list(), .retrieve(), .create(), .update(), 
+    .partial_update(), and .destroy().
     """
     serializer_class = PersonModelSerializer
     queryset = Person.objects.all()
@@ -26,7 +30,10 @@ class PersonModelViewSet(ModelViewSet):
 
 class PhoneModelViewSet(ModelViewSet):
     """
-    A viewset for viewing and editing user instances.
+    select_related obtains all data at one time through multi-table join Association query and improves 
+    performance by reducing the number of database queries. It uses JOIN statements of SQL to optimize 
+    and improve performance by reducing the number of SQL queries. The latter is to solve the problem 
+    in the SQL query through a JOIN statement. 
     """
     serializer_class = PhoneModelSerializer
-    queryset = Phone.objects.all()
+    queryset = Phone.objects.select_related('person')
--- contacts/settings.py
+++ contacts/settings.py
@@ -29,17 +29,26 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+)
+
+THIRD_PARTY_APPS = (
+    'rest_framework',
+)
+
+LOCAL_APPS = (
     "book",
-    "rest_framework"
-]
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
